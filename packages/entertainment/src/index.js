6f3a8028d678816dcd0c423def4158ce7216672f
jalpajora <jezamae.alpajora@gmail.com>
2021-07-20 21:45:57 +0800

    fix: eslint

--- packages/entertainment/src/index.js
+++ packages/entertainment/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default function Entertainment({children}) {
-  return (
+const Entertainment = ({children}) => 
+   (
     <div>
-      <h2>This is entertainment</h2>
+      <h2>{'This is entertainment'}</h2>
       <div>{children}</div>
     </div>
   );
-}
+
+
+export default Entertainment;
